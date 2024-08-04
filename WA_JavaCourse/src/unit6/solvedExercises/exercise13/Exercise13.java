b46a12e64f884515a02bbbfb46dfc6deaf991f59
Jason D <JasDavG@gmail.com>
2024-08-04 18:43:52 +0200

    Exercises Unit 6 exercise 13

---
+++ WA_JavaCourse/src/unit6/solvedExercises/exercise13/Exercise13.java
@@ -0,0 +1,42 @@
+package unit6.solvedExercises.exercise13;
+
+import java.util.Scanner;
+
+public class Exercise13 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter a sentence:");
+		String sentence = sc.nextLine().toLowerCase();
+
+		int[] counter = new int['z' - 'a' + 1]; // This length asignation asures every possible letter has a space by
+												// using their unicode value.
+
+		for (int i = 0; i < sentence.length(); i++) {
+
+			if (Character.isLetter(sentence.charAt(i))) {
+
+				// increments the given character
+				counter[sentence.charAt(i) - 'a']++;
+
+			}
+
+		}
+
+		for (int i = 0; i < 'z' - 'a' + 1; i++) {
+			if (counter[i] > 0) {
+
+				System.out.println(
+						"The letter '" + (char) (i + 'a') + "' appears " + counter[i] + " times, within the sentence.");
+
+			}
+
+		}
+		
+		sc.close();
+
+	}
+
+}
