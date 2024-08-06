0d32e0d79e2a7dfe3495735956145232e469a2dc
Jason D <JasDavG@gmail.com>
2024-08-06 03:17:16 +0200

    Exercise Unit 7 exercise 2

---
+++ WA_JavaCourse/src/unit7/solvedExercises/exercise2/CheckingAccount.java
@@ -0,0 +1,101 @@
+package unit7.solvedExercises.exercise2;
+
+import java.util.Objects;
+
+class CheckingAccount {
+
+	// Attributes
+	private String holderID;
+	private String holderName;
+	private double balance = 0.00;
+
+	// CONSTRUCTORS
+	public CheckingAccount() {
+
+	}
+
+	public CheckingAccount(String holderID,  double balance) {
+		this.holderID = holderID;
+		this.balance = balance;
+	}
+	
+	public CheckingAccount(String holderID, String holderName, double balance) {
+		this(holderID, balance);
+		this.holderName = holderName;
+	}
+
+	// ACCESS METHODS.
+	public String getHolderID() {
+		return holderID;
+	}
+
+	public void setHolderID(String holderID) {
+		this.holderID = holderID;
+	}
+
+	public String getHolderName() {
+		return holderName;
+	}
+
+	public void setHolderName(String holderName) {
+		this.holderName = holderName;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(balance, holderID, holderName);
+	}
+
+	// FUNCTIONAL METHODS.
+	public void withdraw(double amount) {
+
+		if (this.getBalance() >= amount) {
+
+			this.balance = (this.balance - amount);
+			System.out.printf("The amount %.02f has been withdrawn. " + "Current balance: %.02f \n", amount,
+					this.getBalance());
+		} else {
+			System.out.println("The amount surpasses the funds. Cannot withdraw.");
+		}
+
+	}
+
+	public void deposit(double amount) {
+
+		this.balance = (this.getBalance() + amount);
+		System.out.printf("The previous amount available was %.02f\n", (this.balance - amount));
+		System.out.printf("The new amount available is %.02f\n", this.balance);
+	}
+
+	public void showAccountInfo() {
+		System.out.println("------- ACCOUNT INFO -------------------------------");
+		System.out.println("Holder ID: " + this.getHolderID());
+		System.out.println("Holder name: " + this.getHolderName());
+		System.out.println("Account balance: " + this.getBalance());
+	}
+
+	// COMPARISON METHODS.
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CheckingAccount other = (CheckingAccount) obj;
+		return Double.doubleToLongBits(balance) == Double.doubleToLongBits(other.balance)
+				&& Objects.equals(holderID, other.holderID) && Objects.equals(holderName, other.holderName);
+	}
+
+	// GENERIC TOSTRING.
+	@Override
+	public String toString() {
+		return "CheckingAccount [holderID=" + holderID + ", holderName=" + holderName + ", balance=" + balance + "]";
+	}
+
+}
---
+++ WA_JavaCourse/src/unit7/solvedExercises/exercise2/Exercise2.java
@@ -0,0 +1,38 @@
+package unit7.solvedExercises.exercise2;
+
+/*
+ * In the CheckingAccount class, overload the constructors to be able to create objects:
+ * 
+ * With the account holder's DNI and an initial balance.
+ * With the DNI, name, and initial balance.
+ * Write a program that verifies the functionality of the methods.
+ */
+
+public class Exercise2 {
+
+	public static void main(String[] args) {
+
+		// Empty constructor use.
+		CheckingAccount accountJames = new CheckingAccount();
+
+		accountJames.setHolderName("James Sunderland");
+		accountJames.setHolderID("616656376");
+
+		// Constructor with ID and balance.
+		CheckingAccount accountHeather = new CheckingAccount("HM576913", 162437.00);
+
+		// Fully constructed.
+		CheckingAccount accountLisa = new CheckingAccount("LG192837", "Lisa Garland", 99123.20);
+
+		accountJames.showAccountInfo();
+		
+		System.out.println();
+		
+		accountHeather.showAccountInfo();
+		
+		System.out.println();
+		
+		accountLisa.showAccountInfo();
+	}
+
+}
